ebdc475552a2f955cd96ca0a92b8097be0b66684
Mohammad Saimur Rashid <rashidsaimur@icloud.com>
2025-11-01 19:55:43 -0400

    Initial Commit

---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Saimur Rashid on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
